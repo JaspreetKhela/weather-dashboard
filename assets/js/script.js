e5674c96c1a297573ea71fc05e6f5261b4288743
Jaspreet Khela <80941606+JaspreetKhela@users.noreply.github.com>
2021-06-30 03:52:34 -0400

    Added DOM element variables in js

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+// __________
+// DOM element variables
+// __________
+
+// Getting the search form DOM elements
+var form = document.getElementById("form-container");
+var searchButton = document.getElementById("search-button");
+var recentSearches = document.getElementById("button-container");
+
+// Getting the current weather DOM elements
+var selectedCity = document.getElementById("current-city");
+var currentDate = document.getElementById("current-date");
+var currentTemperature = document.getElementById("current-temperature");
+var currentWind = document.getElementById("current-wind");
+var currentHumidity = document.getElementById("current-humidity");
+var currentUVIndex= document.getElementById("current-uv-index");
+
+// Five-Day Forecast
+// Getting the first day's forecast's DOM elements
+var firstDayDate= document.getElementById("first-day-date");
+var firstDayIcon = document.getElementById("first-day-icon");
+var firstDayTemperature = document.getElementById("first-day-temperature");
+var firstDayWind = document.getElementById("first-day-wind");
+var firstDayHumidity = document.getElementById("first-day-humidity");
+
+// Getting the second day's forecast's DOM elements
+var secondDayDate = document.getElementById("second-day-date");
+var secondDayIcon = document.getElementById("second-day-icon");
+var secondDayTemperature = document.getElementById("second-day-temperature");
+var secondDayWind = document.getElementById("second-day-wind");
+var secondDayHumidity = document.getElementById("second-day-humidity");
+
+// Getting the third day's forecast's DOM elements
+var thirdDayDate = document.getElementById("third-day-date");
+var thirdDayIcon = document.getElementById("third-day-icon");
+var thirdDayTemperature = document.getElementById("third-day-temperature");
+var thirdDayWind = document.getElementById("third-day-wind");
+var thirdDayHumidity = document.getElementById("third-day-humidity");
+
+// Getting the forth day's forecast's DOM elements
+var forthDayDate = document.getElementById("forth-day-date");
+var forthDayIcon = document.getElementById("forth-day-icon");
+var forthDayTemperature = document.getElementById("forth-day-temperature");
+var forthDayWind = document.getElementById("forth-day-wind");
+var forthDayHumidity = document.getElementById("forth-day-humidity");
+
+// Getting the fifth day's forecast's DOM elements
+var fifthDayDate = document.getElementById("fifth-day-date");
+var fifthDayIcon = document.getElementById("fifth-day-icon");
+var fifthDayTemperature = document.getElementById("fifth-day-temperature");
+var fifthDayWind = document.getElementById("fifth-day-wind");
+var fifthDayHumidity = document.getElementById("fifth-day-humidity");
